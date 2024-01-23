b3743a6657b590961491146e5e1ded5877755a20
Bruno Akio Branco <brunoakiob@gmail.com>
2024-01-23 14:06:31 -0300

    v2

--- src/controllers/FormatController.js
+++ src/controllers/FormatController.js
@@ -1,20 +1,47 @@
-const Format = require('../models/format');
-
+const { response } = require("express");
+const Format = require("../models/format");
+const { user, PRIVATE_KEY, tokenValited } = require("../security/auth");
+const jsonwebtoken = require("jsonwebtoken");
 module.exports = {
-    async format(req, res){
-        try {
-            const { numero, tipo } = req.body;
-    
-            const numeroFormatado = Format(numero, tipo);
-    
-            if (numeroFormatado) {
-                res.json({ numeroFormatado });
-            } else {
-                res.status(400).json({ erro: 'Formato inválido ou tipo não suportado.' });
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ erro: 'Erro interno do servidor.' });
-        }
+  async format(req, res) {
+    try {
+      const { numero, tipo } = req.body;
+
+      const numeroFormatado = Format(numero, tipo);
+
+      if (numeroFormatado) {
+        res.json({ numeroFormatado });
+      } else {
+        res
+          .status(400)
+          .json({ erro: "Formato inválido ou tipo não suportado." });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ erro: "Erro interno do servidor." });
     }
-}
+  },
+  async auth(req, res) {
+    const [, hash] = req.headers.authorization?.split(" ") || [" ", " "];
+    const [email, password] = Buffer.from(hash, "base64").toString().split(":");
+
+    try {
+      const correctPassword =
+        email === "format@vitru.com" && password === "Vitru#2024";
+
+      if (!correctPassword)
+        return response.status(401).send("Password or E-mail incorrect!");
+
+      const token = jsonwebtoken.sign(
+        { user: JSON.stringify(user) },
+        PRIVATE_KEY,
+        { expireIn: "60m" }
+      );
+
+      return res.status(200).json({ data: { user, token } });
+    } catch (error) {
+      console.log(error);
+      return res.send(error);
+    }
+  },
+};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
 const routes = require('./router');
+const jsonwebtoken = require('jsonwebtoken');
+const { user, PRIVATE_KEY, tokenValited} = require('./security/auth');
 
 app.use(express.json());
 app.use(routes);
